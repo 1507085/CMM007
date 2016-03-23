4a940095abf187e1bb627c1502fc4d0478501ad1
1507085 <totious500@gmail.com>
2016-03-23 18:52:51 +0000

    a

---
+++ Week5/HOME.php
@@ -0,0 +1,17 @@
+
+/**
+ * Created by PhpStorm.
+ * User: 1507085
+ * Date: 23/03/2016
+ * Time: 18:50
+ */
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+<h1>Hello</h1>
+</body>
+</html>
