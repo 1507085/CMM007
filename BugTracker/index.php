cbe982ae3a983f7017a2f5c6ff81edf1645e0c0d
1507085 <totious500@gmail.com>
2016-04-25 10:54:22 +0100

    a

---
+++ BugTracker/index.php
@@ -0,0 +1,50 @@
+
+/**
+ * Created by PhpStorm.
+ * User: 1507085
+ * Date: 25/04/2016
+ * Time: 10:39
+ */
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="stle.css" type="text/css"/>
+</head>
+<body>
+<header>
+    <div id="hed">
+        <img id="log" src="logo.png" width="100"/>
+        <h1><strong>BugTracker</strong><br>
+            Keeping track of all the pesky little bugs</h1>
+    </div>
+</header>
+<main>
+    <section>
+        <nav>
+            <ul>
+                <li><a href="addbugs.php & showbug.php">All bugs Items</a></li>
+                <li><a href="addbugs.php & showbug.php">Android Bugs</a></li>
+                <li><a href="addbugs.php & showbug.php">iOS Bugs</a></li>
+                <li><a href="addbugs.php & showbug.php">Window Bugs</a></li>
+                <li><a href="addbugs.php & showbug.php">Insert Bug</a></li>
+            </ul>
+        </nav>
+    </section>
+    <section>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do<br>
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim<br>
+            ad minim veniam, quis nostrud exerciation ullamco laboris nisi ut<br>
+            aliquip ex ea commado consequat. Duis aute irure dolor in <br>
+            reprehendenrit in voluptate velit esse cillum dolore eu fugiat nulla<br>
+            pariatur. Excepteur sint occaecat cupidatat non proidnt. sunt in culpa<br>
+            qui officia deserunt mollit anim id est laborum </p>
+    </section>
+</main>
+<footer>
+    <P>Design by Emmanuel Onumah. 2016 </P>
+</footer>
+</body>
+</html>
