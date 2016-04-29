ed2df3ab4e02fc617cfd1649d5aec02a0e1ac88f
totio <totious500@gmail.com>
2016-04-29 11:47:59 +0100

    a

---
+++ addsch.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: totio
+ * Date: 29/04/2016
+ * Time: 11:21
+ */
+
+//get connection
+include(connection.php);
+//send data to the table
+
+if ($_POST["submit"]){
+    $schname="";
+    $schaddress="";
+
+    //create sql query to send the data
+    $sql="INSERT  INTO myskul (schname,schaddress) VALUES ($schname,$schaddress)";
+
+    //connect query to database
+    mysqli_query($db,$sql);
+    if (mysqli_query($db,$sql)){
+
+        echo "data added";}
+        else{
+            echo "data not added";
+
+
+    }
+
+}
